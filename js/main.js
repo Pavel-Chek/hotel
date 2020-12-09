40afd090a34488669feefd931b0e693acf3b2a94
Pavel-Chek <chp0705@gmail.com>
2020-12-09 13:24:22 +0300

    created: php and jq form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.swiper-container', {
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.swiper-container', {
     loop: true,
 
     keyboard: {
@@ -25,8 +26,39 @@ var hotelSlider = new Swiper('.swiper-container', {
     },
   });
 
-  var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener("click", function () {
-    console.log("Click")
-    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
-  })
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible")
+  });
+  var modalButton = $('[data-toggle=modal]');
+  var CloseModalButton = $('.modal_close');
+  var modalOverlay = $(".modal_overlay");
+  var modalDialog = $(".modal_dialog");
+  modalButton.on('click', openModal);
+  CloseModalButton.on('click', closeModal);
+
+  $(document).on('keydown', function(event) {
+    if (event.code === 'Escape') {
+      console.log('keydown');
+        closeWindow();
+    }
+});
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal_overlay").addClass("modal_overlay--visible");
+    $(targetModal).find(".modal_dialog").addClass("modal_dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    modalOverlay.removeClass("modal_overlay--visible");
+    modalDialog.removeClass("modal_dialog--visible");
+  }
+
+  function closeWindow(event) {
+    modalOverlay.removeClass("modal_overlay--visible");
+    modalDialog.removeClass("modal_dialog--visible");
+  }
+
+});
---
+++ send.php
@@ -0,0 +1,58 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Новое письмо Best tour plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message<br>
+<b>email:</b><br>$email<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'menedzher.email@mail.ru'; // Логин на почте
+    $mail->Password   = 'Qwerty123441235'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('menedzher.email@mail.ru', 'Менеджер'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('ya8314@yandex.ru'); // Ещё один, если нужен
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header("location: thankyou.html");
